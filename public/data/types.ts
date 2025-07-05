29c8534557d0ff1722f64d12ef06c9e16fde2683
Edward Cooper <edcooper.dev@gmail.com>
2025-07-05 18:29:38 -0500

    create data structure file

---
+++ public/data/types.ts
@@ -0,0 +1,16 @@
+export type RowData = {
+    BATTER_ID: string;
+    BATTER: string;
+    PITCHER_ID: string;
+    PITCHER: string;
+    GAME_DATE: string;
+    LAUNCH_ANGLE: number;
+    EXIT_SPEED: number;
+    EXIT_DIRECTION: number;
+    HIT_DISTANCE: number;
+    HANG_TIME: number;
+    HIT_SPIN_RATE: number;
+    PLAY_OUTCOME: string;
+    VIDEO_LINK: string;
+  };
+  
